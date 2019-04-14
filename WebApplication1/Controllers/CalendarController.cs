363591acbd993c2925708c684784c59f333b4cb5
pulina-paulina <ekaterina.paulina@yandex.ru>
2019-04-14 12:19:13 +0300

    calendar.today

---
+++ WebApplication1/Controllers/CalendarController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Today()
+        {
+            return View();
+        }
+    }
+}
